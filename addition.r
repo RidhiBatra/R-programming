c2995a16c05f92af5bef4b61035b6ed706f0099d
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-05-25 12:14:57 +0530

    Add files via upload

---
+++ addition.r
@@ -0,0 +1,20 @@
+#write a program to add 2 numbers using integer input
+
+{
+  y<- as.integer(readline(prompt="Enter num1: "))
+x<- as.integer(readline(prompt = "Enter num2: "))
+cat(x+y)
+}
+class(y)
+#as.logical->  to convert it to boolean
+#as.character-> to convert it to char.
+#we can also add a numeric and integer variable. by default priority onw will take the value...
+
+#write a program to add 2 complex numbers.
+{
+a<-as.complex(readline("enter num1 = "))
+b<-as.complex(readline("enter num2 = "))
+z= a+b
+z=as.complex(z)
+print(z)
+}
