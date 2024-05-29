5a3f529a92839bd1d232ab4c37008cbabd15a64e
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-05-29 11:16:30 +0530

    Add files via upload

---
+++ conditionalstatements.r
@@ -0,0 +1,17 @@
+#conditional Statements
+{
+  n = as.Integer(readline("Enter a number: "))
+  if(n==0)
+  {
+    cat("zero!!")
+  }
+  else if(n>0)
+  {
+    cat("Positive Number..")
+  }
+  else
+  {
+    cat("Negative Number..")
+  }
+  
+}
