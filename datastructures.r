02bef829dbf3752b94852a99e7762289b24da5e6
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-06-06 17:58:21 +0530

    Add files via upload

---
+++ datastructures.r
@@ -0,0 +1,14 @@
+#DATA STRUCTURES
+#1. VECTOR
+#2. FACTOR
+#3. MATRIX
+#4. DATAFRAME 
+#5. ARRAY
+#6. LIST
+a=c(10,20,30,40,50)
+class(a)
+typeof(a)
+
+#how to give priority in different types
+#vector can hold multiple values at the same time but if multiple values are of different data types we can convert it into one datatype by conversion
+#character->complex->numeric....
