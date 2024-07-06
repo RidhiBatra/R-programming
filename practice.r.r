2c4fd7ba75a92970b43e88f3db0122b167fca03c
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-07-06 10:43:36 +0530

    Add files via upload

---
+++ practice.r.r
@@ -0,0 +1,9 @@
+{
+  y<- as.numeric(readline(prompt="Enter num1: "))
+  x<- as.integer(readline(prompt = "Enter num2: "))
+  cat(x+y)
+}
+class(x+y)
+typeof(x+y)
+# Creating a 3-dimensional array
+my_array <- array(1:24, dim = c(2, 3, 4))
