9098bf0071be8553d0b72ebed7819a1e526b8664
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-07-13 10:29:57 +0530

    Add files via upload

---
+++ strings.r
@@ -0,0 +1,20 @@
+{x= readline("enter a string: ")
+nchar(x) 
+}     #count the number of characters in a string
+substr("shreya",1,5)
+strsplit("hello world"," ")
+toupper("hello")
+tolower("Yellow")
+strtrim("this is my world",8)
+{
+y = readline("enter name: ")
+t= as.integer(readline("enter age: "))
+u = as.numeric(readline("enter percentage: "))
+sprintf("%s %d %f",y,t,u)
+}
+
+v1 = c("Ridhi" , "Shreya" , "Aarchie", "Priya", "Manya")
+grep("a+",v1)    #a kon kon se name mai hai
+
+d4= file.choose()
+#apply(d4,2,sort)
